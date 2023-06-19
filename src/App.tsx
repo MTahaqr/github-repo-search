aebdb53adfa6e97d5433e82da13a52172da94a1d
MTahaqr <tahaqr28@outlook.com>
2023-06-19 13:16:31 +0500

    updagte route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import BookmarkPage from './containers/BookmarkPage';
-import SearchPage from './containers/SearchPage';
-import MainLayout from './Layouts/MainLayout';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import BookmarkPage from './containers/BookmarkPage'
+import SearchPage from './containers/SearchPage'
+import MainLayout from './Layouts/MainLayout'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<SearchPage />} />
-          <Route path="/bookmark" element={<BookmarkPage />} />
+        <Route path='/' element={<MainLayout />}>
+          <Route index element={<SearchPage />} />
+          <Route path='bookmark' element={<BookmarkPage />} />
         </Route>
       </Routes>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
